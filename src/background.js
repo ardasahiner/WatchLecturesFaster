21eca399c033b30d45ae3f0a3c061062eabc914a
Kunal Gosar <kunal.gosar@berkeley.edu>
2017-02-20 03:14:03 -0800

    Configured and Working

--- src/background.js
+++ src/background.js
@@ -1,1 +1,10 @@
 // Background Script
+
+chrome.runtime.onMessage.addListener(function (message, sender, callback) {
+    if (message === 'enable_page_action') {
+        chrome.pageAction.show(sender.tab.id);
+    }
+});
+
+console.log("test");
+document.getElementById('eow-title').innerHTML = "fjafjiaossdfja";
--- src/content.js
+++ src/content.js
@@ -1,1 +1,14 @@
-// Content Script
+function titleUpdate() {
+     document.getElementById('eow-title').innerHTML = "Change Title to test Response";
+     console.log("test");
+
+     // Element exists, remove the event listeners so we don't run this twice
+     document.removeEventListener('DOMNodeInserted', titleUpdate);
+     document.removeEventListener('DOMContentLoaded', titleUpdate);
+
+     chrome.runtime.sendMessage('enable_page_action', function () {});
+}
+
+// event listeners
+document.addEventListener('DOMNodeInserted', titleUpdate);
+document.addEventListener('DOMContentLoaded', titleUpdate);
