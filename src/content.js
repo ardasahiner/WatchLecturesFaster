93804d199abf80d8173918c6d42a08d2bc4dc802
Kunal Gosar <kunal.gosar@berkeley.edu>
2017-02-20 03:44:42 -0800

    slider to control playbackRate

--- src/content.js
+++ src/content.js
@@ -1,9 +1,18 @@
 function titleUpdate() {
-     document.getElementsByTagName("video")[0].playbackRate = 3;
 
-     // Element exists, remove the event listeners so we don't run this twice
      document.removeEventListener('DOMNodeInserted', titleUpdate);
      document.removeEventListener('DOMContentLoaded', titleUpdate);
+
+     var x = document.createElement("INPUT");
+     x.setAttribute("type", "range");
+     x.setAttribute("min", "0.1");
+     x.setAttribute("max", "3");
+     x.setAttribute("step", "0.2");
+     x.addEventListener('input', function(evt) {
+          document.getElementsByTagName("video")[0].playbackRate = this.value;
+          console.log(this.value);
+     });
+     document.getElementById("placeholder-player").appendChild(x);
 
      chrome.runtime.sendMessage('enable_page_action', function () {});
 }
